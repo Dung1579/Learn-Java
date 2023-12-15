b595541b369890c44087ef7b3d395b18c4a3dd97
dung nguyen <thuydung99na@gmail.com>
2023-12-15 22:25:11 +0700

    learnJava-lesson1

--- Main.java
+++ Main.java
@@ -1,7 +1,121 @@
-public class Main {
+//Syntax + Output
+// public class Main {
+//     public static void main(String[] args) {
+//       System.out.println("Hello World");
+//     }
+//   }
+
+// Variables
+  /*public class Main {
     public static void main(String[] args) {
-      double mydouble = 9.78d;
-      int myint = (int) mydouble;
-      System.out.println(myint);
+      String FullName = "Nguyen Thi Dung";
+      System.out.println(FullName);
     }
   }
+*/
+
+/*
+  public class Main {
+    public static void main(String[] args) {
+      String FirstName = "Dung ";
+      String LastName = "Nguyen";
+      String FullName = FirstName + LastName;
+      System.out.println(FullName);
+    }
+  }
+*/
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int MyAge = 24;
+  //     System.out.println(MyAge);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     String Xom = "Xom 5, ";
+  //     String Xa = "Dien Dong, ";
+  //     String Huyen = "Dien Chau, ";
+  //     String Tinh = "Nghe An";
+  //     String MyAdress = Xom + Xa + Huyen + Tinh;
+  //     System.out.println(MyAdress);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     char Xom = '5';
+  //     System.out.println(Xom);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     float Diem = 9.75f;
+  //     System.out.println(Diem);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int x = 15;
+  //     x = 20;
+  //     System.out.println(x);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int x = 15;
+  //     int y = 20;
+  //     int z = x + y;
+  //     boolean Status = true;
+  //     System.out.println(Status); //Output: true
+  //     System.out.println(z); //Output: 35
+  //   }
+  // }
+
+  // Print Variables
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     String name = "Dung";
+  //     System.out.println("Hello " + name);
+  //   }
+  // }
+
+
+  //Declare Multiple Variables: Khai bao nhieu bien
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int x = 10, y = 20, z = 15;
+  //     System.out.println(x + y + z);
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int x, y, z;
+  //     x = y = z = 50;
+  //     System.out.println(x + y + z);
+  //   }
+  // }
+
+  // Type Casting (Truyền type): Type casting is when you assign a value of one primitive data type to another type.(Khi muốn gán type của 1 giá trị nguyên thủy sang 1 type khác)
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     int Myint = 9;
+  //     double Mydouble = Myint; //Automatic casting: int to double: Covert từ type nhỏe hơn sang type lớn hơn (byte -> short -> char -> int -> long -> float -> double)
+  //     System.out.println(Myint); //Output: 9
+  //     System.out.println(Mydouble);  //Output: 9.0
+  //   }
+  // }
+
+  // public class Main {
+  //   public static void main(String[] args) {
+  //     double Mydouble = 9.75d;
+  //     int Myint = (int) Mydouble;  //Manual casting: double to int: Convert từ type lớn hơn sang type nhỏ hơn (double -> float -> long -> int -> char -> short -> byte)
+  //     System.out.println(Myint);  //Output: 9
+  //     System.out.println(Mydouble);  //Output: 9.75
+  //   }
+  // }
